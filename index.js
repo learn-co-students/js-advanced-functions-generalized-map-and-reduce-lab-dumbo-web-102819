e33e7083eabdff60bcf4d23fea648413c46e8478
Frank Gerold <FrankETSGerold@gmail.com>
2019-12-16 14:24:43 -0500

    d

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ function map (array, func) {
 }
 
 // function reduce (array, func, total = 0) {
-//   for (var i = 0; i < array.length; i++) {
+//   for (let i = 0; i < array.length; i++) {
 //     total = func(array[i], total)
 //   }
 //   return total
 // }
 
+
+// there must be a better way to choose whether or not to iterate from index 0 than this! solution:
 function reduce(src, cb, starting){
   let r = (!!starting) ? starting : src[0]
   let i = (!!starting) ? 0 : 1
