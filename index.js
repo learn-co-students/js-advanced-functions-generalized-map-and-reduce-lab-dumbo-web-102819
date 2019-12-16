6cb5b167c7115b6923f7b84884100e8c9be1bb0c
Frank Gerold <FrankETSGerold@gmail.com>
2019-12-16 14:18:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+function map (array, func) {
+  let newArr = [];
+  for (var i = 0; i < array.length; i++) {
+    newArr.push(func(array[i]))
+  }
+  return newArr
+}
+
+// function reduce (array, func, total = 0) {
+//   for (var i = 0; i < array.length; i++) {
+//     total = func(array[i], total)
+//   }
+//   return total
+// }
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
